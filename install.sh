6200cd66c6d85b99b434ac28c80ebae87d52bc70
MC2BP <service@MC2BP.ch>
2020-11-07 19:15:26 +0100

    added picom for transparent code and terminal

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ cp ./i3/config ~/.config/i3/
 cp ./polybar/config ~/.config/polybar/
 cp ./polybar/launch.sh ~/.config/polybar/
 cp ./alacritty/alacritty.yml ~/.config/alacritty/
+cp ./picom/picom.conf ~/.config/picom/
